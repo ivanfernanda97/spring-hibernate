556065b60bc4059d4df31117b76d59cc26b2b37f
Vanxyr <113080841+Vanxyr@users.noreply.github.com>
2022-12-07 08:00:24 +0700

    Add files via upload

---
+++ 23-spring-data-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,76 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	// define field
+	private EmployeeRepository employeeRepository;
+	
+	// set up constructor injection
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+		employeeRepository = theEmployeeRepository;
+	}
+		
+	@Override
+	public List<Employee> findAll() {
+		return  employeeRepository.findAll();
+
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		
+		if(result.isPresent()) {
+			theEmployee = result.get();
+		}
+		else {
+			// we didn't find the employee 
+			throw new RuntimeException("Did not found employe id - " + theId);
+		}
+		
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		employeeRepository.deleteById(theId);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
